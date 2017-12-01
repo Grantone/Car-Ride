fd582e7e4ec01a6a0abefaf2d22385978386575f
Granton Mchana <gmchana254@gmail.com>
2017-12-01 17:50:26 +0300

    database fields

--- carpool/ride/models.py
+++ carpool/ride/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    picture = modelsImageField(upload_to='profiles/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    work = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username

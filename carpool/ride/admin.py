6ac9b2f8e90a0991a336c43522224329752f1c00
Granton Mchana <gmchana254@gmail.com>
2017-12-03 16:58:41 +0300

    created django superuser

--- carpool/ride/admin.py
+++ carpool/ride/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, DriverInfo, Vehicle, Request, VehicleSharing,
+from .models import Profile, DriverInfo, Vehicle, Request, VehicleSharing
 
 # Register your models here.
 admin.site.register(Profile)
